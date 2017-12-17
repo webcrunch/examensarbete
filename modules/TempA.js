83cf0dcb809e401d03734979eb76018baf7edd7c
Jarl Lindquist <jarllindquist@gmail.com>
2017-12-17 20:45:20 +0100

    creating an temporary security data file

---
+++ modules/TempA.js
@@ -0,0 +1,39 @@
+var path = require("path");
+var appRoot = path.normalize(__dirname + "/../" +  '/');
+
+   module.exports = settings = {
+  		  /*server information */
+ 	endpoint : '*',		
+ 	webRoot: 'www',		
+ 	appRoot : appRoot,
+ 	fileRoot: appRoot + 'www',		
+ 	indexFile: '/index.html',		
+ 	port: 3002,		// insert the port the server listens to 
+  /*Amazon information*/
+ 	signatureVersion: 'v4',		// 
+  secretAccessKey: '',		// insert secret Access  Key
+  accessKeyId: '',		// instert secret Key Id
+  region: '',		// Witch region
+
+  bucket: '',   // the name of the bucket     
+   acl: '',      
+   key: '',   
+  secret: '',   
+  bucketPath : "",
+
+
+
+/*Database information*/
+  host     : '',		// what host 
+ 	user     : '',		// what user?? 
+ 	password : '',		// whitch password
+ 	database : '',		// the name of the database
+ 	protocol: '',		  // sort of protocoll (used by sequlize)
+ 	portServer: 	// Witch port to database 
+ 	
+
+
+  			  	
+  		    };
+  
+
